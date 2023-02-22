70091dbb9751747df10fb9f5442dfd5143a0e396
luisjaas <luisjaas98@gmail.com>
2023-02-22 02:42:05 +0000

    home

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logonav from "../../img/STICKERS CRUZANGELO.png"
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
+		<nav className="navbar bg-dark d-flex justify-content-center" data-bs-theme="dark">
+			<img src={logonav} className="logonav"></img>
 		</nav>
 	);
 };
