a902432411e7106727154c5f4f5f7bdc1d630ac1
danielmoret <dmoret17@gmail.com>
2023-02-25 15:39:54 -0400

    funcionando back y front al iniciar sesion

--- src/front/js/pages/administrador.js
+++ src/front/js/pages/administrador.js
@@ -1,27 +1,53 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { Context } from "../store/appContext";
+import toast, { Toaster } from "react-hot-toast";
 
 import "../../styles/administrador.css";
 
 export const Administrador = () => {
+  const { actions } = useContext(Context);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const sendData = async (event) => {
+    event.preventDefault();
+    const login = await actions.login(email, password);
+    if (login) {
+      toast.success("Logueado correctamente");
+    } else {
+      toast.error("Credenciales invalidas");
+    }
+  };
+
   return (
     <div className="container-login">
+      <Toaster />
       <div className="login text-center">
         <h1>Panel Administrativo</h1>
-        <form>
+        <form onSubmit={sendData}>
           <div className="form-group">
-            <label className="form-label" for="email">
+            <label className="form-label" htmlFor="email">
               <b>Usuario:</b>
             </label>
-            <input className="form-control" type="email" id="email" required />
+            <input
+              className="form-control"
+              type="email"
+              id="email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+              required
+            />
           </div>
           <div className="form-group">
-            <label className="form-label" for="password">
+            <label className="form-label" htmlFor="password">
               <b>Contraseña:</b>
             </label>
             <input
               className="form-control"
               type="password"
               id="password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
               required
             />
           </div>
