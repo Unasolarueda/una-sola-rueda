f65d743b34bbc718f3408b17bcbf56402ff23ee5
danielmoret <dmoret17@gmail.com>
2023-02-25 13:07:45 -0400

    se creo endpoints user y login

--- src/api/models.py
+++ src/api/models.py
@@ -12,16 +12,36 @@ class Role(Enum):
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    role = db.Column(db.Enum(Role), nullable=False, default="raffler")
+    password = db.Column(db.String(120), unique=False, nullable=False)
+    salt = db.Column(db.String(80), unique=False, nullable=False)
+    role = db.Column(db.Enum(Role), nullable=False)
 
+    def __init__(self,**kwargs):
+        self.email = kwargs['email']
+        self.password = kwargs['password']
+        self.salt = kwargs['salt']
+        self.role = kwargs['role']
+    
     def __repr__(self):
         return f'<User {self.email}>'
+
+    @classmethod
+    def create(cls, **kwargs):
+        new_user = cls(**kwargs)
+        db.session.add(new_user)
+
+        try:
+            db.session.commit()
+            return new_user
+        except Exception as error:
+            db.session.rollback()
+            raise Exception(error.args[0],400)
+
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            "role": self.role
+            "role": self.role.value
             # do not serialize the password, its a security breach
         }
