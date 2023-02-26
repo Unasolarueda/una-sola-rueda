c2e1e4c21b23115a135de60f224119bde7995f94
danielmoret <dmoret17@gmail.com>
2023-02-26 09:57:26 -0400

    se visualizan correctamente los usuarios desde la BD

--- src/front/js/pages/user.js
+++ src/front/js/pages/user.js
@@ -1,42 +1,41 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
+import { Context } from "../store/appContext";
 import "../../styles/user.css";
 
 export const User = () => {
+  const { store, actions } = useContext(Context);
+  useEffect(() => {
+    if (store.users && store.users.length > 0) return;
+    actions.getUsers();
+  }, []);
+
+  console.log(store.users);
+
   return (
-    <div className="container table-user  mt-3">
+    <div className="container table-user  mt-4">
       <div className="d-flex justify-content-between">
         <h2>Usuarios</h2>
         <button className="btn btn-success text-dark fw-bold">
           Añadir Usuario
         </button>
       </div>
-      <table class="table">
+      <table className="table">
         <thead>
           <tr>
             <th scope="col">#</th>
             <th scope="col">Username</th>
-            <th scope="col">Email</th>
-            <th scope="col">Acción</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td colspan="2">Larry the Bird</td>
-            <td>@twitter</td>
-          </tr>
+          {store.users.map((user, index) => (
+            <tr key={user.id}>
+              <th scope="row">{index + 1}</th>
+              <td>{user.email}</td>
+              <td>
+                <button className="btn btn-danger">Eliminar</button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -2,18 +2,8 @@ const getState = ({ getStore, getActions, setStore }) => {
   return {
     store: {
       message: null,
-      demo: [
-        {
-          title: "FIRST",
-          background: "white",
-          initial: "white",
-        },
-        {
-          title: "SECOND",
-          background: "white",
-          initial: "white",
-        },
-      ],
+      token: sessionStorage.getItem("token") || null,
+      users: [],
     },
     actions: {
       login: async (email, password) => {
@@ -28,48 +18,47 @@ const getState = ({ getStore, getActions, setStore }) => {
           }),
         };
         try {
-          const response = await fetch(`http://127.0.0.1:3001/api/login`, opts);
+          const response = await fetch(
+            `${process.env.BACKEND_URL}/api/login`,
+            opts
+          );
 
           if (!response.ok) {
             return false;
           }
           const data = await response.json();
-          console.log(data);
+          sessionStorage.setItem("token", data.token);
+          setStore({ token: data.token });
           return true;
         } catch (error) {
           console.error("There was been an error login in");
         }
       },
-      // Use getActions to call a function within a fuction
-      exampleFunction: () => {
-        getActions().changeColor(0, "green");
-      },
 
-      getMessage: async () => {
-        try {
-          // fetching data from the backend
-          const resp = await fetch(process.env.BACKEND_URL + "/api/hello");
-          const data = await resp.json();
-          setStore({ message: data.message });
-          // don't forget to return something, that is how the async resolves
-          return data;
-        } catch (error) {
-          console.log("Error loading message from backend", error);
-        }
-      },
-      changeColor: (index, color) => {
-        //get the store
+      getUsers: async () => {
         const store = getStore();
+        const opts = {
+          headers: {
+            Authorization: `Bearer ${store.token}`,
+          },
+        };
 
-        //we have to loop the entire demo array to look for the respective index
-        //and change its color
-        const demo = store.demo.map((elm, i) => {
-          if (i === index) elm.background = color;
-          return elm;
-        });
+        try {
+          const response = await fetch(
+            `${process.env.BACKEND_URL}/api/user`,
+            opts
+          );
+          if (!response.ok) {
+            alert("no se pudo obteber usuarios");
+            return false;
+          }
 
-        //reset the global store
-        setStore({ demo: demo });
+          const data = await response.json();
+          setStore({ users: data });
+          return true;
+        } catch (error) {
+          console.error(error);
+        }
       },
     },
   };
