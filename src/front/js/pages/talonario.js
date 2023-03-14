7511a1fc4aa51e3767fff2248f2b50fbc755635f
danielmoret <dmoret17@gmail.com>
2023-03-13 20:46:18 -0400

    fix modal in talonario

--- src/front/js/pages/talonario.js
+++ src/front/js/pages/talonario.js
@@ -38,60 +38,56 @@ export const Talonario = () => {
 
   return (
     <>
-      <div className="text-center mt-5 mb-5">
-        <h1>Tickets</h1>
-
-        <div className="talonario d-flex flex-wrap justify-content-center p-2 gap-2">
-          <div data-bs-toggle="modal" data-bs-target="#exampleModal2"></div>
-        </div>
-      </div>
-      <div className="text-center mt-5 mb-5">
-        <h1>Tickets</h1>
-        <div className="talonario d-flex flex-wrap justify-content-center p-2 gap-2  text-white">
-          {store.tickets.map((numero, index) => (
-            <div
-              value={`${numero.status} ${numero.value}`}
-              key={index}
-              className={
-                numero.status == "reservado"
-                  ? "numero_reservado numero"
-                  : numero.status == "pagado"
-                  ? "numero_pagado numero"
-                  : "numero numero_disponible"
-              }
-              data-bs-toggle="modal"
-              data-bs-target="#exampleModal2"
-              onClick={(e) =>
-                setTicketSelected({
-                  value: numero.value,
-                  numero: numero.numero,
-                  status: numero.status,
-                })
-              }
-            >
-              {numero.value}
+      {store.talonarios.length > 0 ? (
+        <>
+          <div className="text-center mt-5 mb-5 text-white">
+            <h1>Tickets</h1>
+            <div className="talonario d-flex flex-wrap justify-content-center p-2 gap-2  text-white">
+              {store.tickets.map((numero, index) => (
+                <div
+                  value={`${numero.status} ${numero.value}`}
+                  key={index}
+                  className={
+                    numero.status == "reservado"
+                      ? "numero_reservado numero"
+                      : numero.status == "pagado"
+                      ? "numero_pagado numero"
+                      : "numero numero_disponible"
+                  }
+                  data-bs-toggle="modal"
+                  data-bs-target="#exampleModalToggle"
+                  onClick={(e) =>
+                    setTicketSelected({
+                      value: numero.value,
+                      numero: numero.numero,
+                      status: numero.status,
+                    })
+                  }
+                >
+                  {numero.value}
+                </div>
+              ))}
             </div>
-          ))}
+          </div>
+        </>
+      ) : (
+        <div className="text-center min-vh-100 d-flex justify-content-center align-items-center">
+          <NewTalonario />
         </div>
-      </div>
+      )}
 
-      <div className="text-center min-vh-100 d-flex justify-content-center align-items-center">
-        <NewTalonario />
-      </div>
-
-      {/*Primer modal */}
-
+      {/* Primer Modal */}
       <div
         className="modal fade"
-        id="exampleModal2"
-        tabIndex={-1}
-        aria-labelledby="exampleModal2Label"
+        id="exampleModalToggle"
         aria-hidden="true"
+        aria-labelledby="exampleModalToggleLabel"
+        tabIndex="-1"
       >
-        <div className="modal-dialog">
+        <div className="modal-dialog modal-dialog-centered">
           <div className="modal-content">
             <div className="modal-header">
-              <h1 className="modal-title fs-5" id="exampleModal2Label">
+              <h1 className="modal-title fs-5" id="exampleModalToggleLabel">
                 Ticket {ticketSelected.value}
               </h1>
               <button
@@ -108,7 +104,7 @@ export const Talonario = () => {
               {ticketSelected.status !== "disponible" && (
                 <button
                   className="btn btn-outline-dark btn-closed"
-                  data-bs-target="#exampleModal2Toggle2"
+                  data-bs-target="#exampleModalToggle2"
                   data-bs-toggle="modal"
                   onClick={(e) =>
                     actions.infoTicket(
@@ -121,46 +117,22 @@ export const Talonario = () => {
                 </button>
               )}
             </div>
-            <div className="modal-footer">
-              <button
-                type="button"
-                className="btn btn-outline-dark btn-closed"
-                data-bs-dismiss="modal"
-              >
-                Volver
-              </button>
-
-              <button
-                type="button"
-                className="btn btn-success btn-liberar"
-                data-bs-dismiss="modal"
-              >
-                Liberar Ticket
-              </button>
-
-              <button
-                type="button"
-                className="btn btn-primary register"
-                data-bs-dismiss="modal"
-              >
-                Marcar como pagado
-              </button>
-            </div>
           </div>
         </div>
       </div>
-      {/*Segundo modal */}
+
+      {/* Segundo Modal */}
       <div
         className="modal fade"
-        id="exampleModal2Toggle2"
+        id="exampleModalToggle2"
         aria-hidden="true"
-        aria-labelledby="exampleModal2ToggleLabel2"
-        tabIndex={-1}
+        aria-labelledby="exampleModalToggleLabel2"
+        tabIndex="-1"
       >
         <div className="modal-dialog modal-dialog-centered">
           <div className="modal-content">
             <div className="modal-header">
-              <h1 className="modal-title fs-5" id="exampleModal2ToggleLabel2">
+              <h1 className="modal-title fs-5" id="exampleModalToggleLabel2">
                 Información del ticket {ticketSelected.value}
               </h1>
               <button
@@ -186,7 +158,7 @@ export const Talonario = () => {
             <div className="modal-footer">
               <button
                 className="btn btn-outline-dark btn-closed"
-                data-bs-target="#exampleModal2Toggle"
+                data-bs-target="#exampleModalToggle"
                 data-bs-toggle="modal"
               >
                 Volver
