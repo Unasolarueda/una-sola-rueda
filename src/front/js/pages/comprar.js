af1c067dd491de35bc8c58995c6a1a0dd344e323
Unasolarueda <151055259+Unasolarueda@users.noreply.github.com>
2024-01-04 17:12:11 -0400

    Update comprar.js

--- src/front/js/pages/comprar.js
+++ src/front/js/pages/comprar.js
@@ -384,7 +384,14 @@ export const Comprar = () => {
                       Nota:con el numero podra hacer el zelle sin usar correo
                     </p>
                   </div>
-                )
+                ) : paymentMethod == 'binance' ? (
+                  <div className="datos-transferencia">
+                  <p>ASDASD </p>
+                  <p>WERWER </p>
+                  <p>
+                      Nota:con el ID BINANCE podra hacer el pago sin usar correo
+                    </p>
+                  </div>
               )}
               {errors.paymentMethod && (
                 <div className="alert alert-danger p-1" role="alert">
