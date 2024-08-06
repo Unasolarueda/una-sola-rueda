52d43de758ea7dd5ac513a49618c208d2ad6aeb1
Laymont Arratia <laymont@gmail.com>
2024-08-05 20:43:55 -0400

    new cloud name

--- src/front/js/component/cloudinary.js
+++ src/front/js/component/cloudinary.js
@@ -5,7 +5,8 @@ class CloudinaryUploadWidget extends Component {
   static contextType = Context;
   componentDidMount() {
    
-    const cloudName = "du8wenys3";
+    //const cloudName = "du8wenys3";
+      const cloudName = "dvvp6cy93"
     const uploadPreset = "cdhgx4ba";
     //const uploadPreset = process.env.UPLOAD_PRESET;
    
